1977858fa603148fc47fe8fae4d0e93a2cd0be87
ValeOlivares <valentinaolivars85@gmail.com>
2023-05-20 17:36:55 -0400

    style(Home): initial styles to home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter } from 'react-router-dom'
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/Home/Home';
 
+import './main.css';
+
 function App() {
   return (
     <BrowserRouter>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { getAmiibos, addToCart } from '../../store/actions/amiibosActions';
+
+import Button from '@mui/material/Button';
+
+import './home.css'
 
 class Home extends React.Component {
   componentDidMount(){
@@ -10,16 +15,26 @@ class Home extends React.Component {
   render() {
     const { amiibos, cartItems } = this.props.amiibos;
     console.log("cart", cartItems)
-    
+
     const handleAmiiboClick = (item) => {
       this.props.addToCart(item)
     };
 
     return (
-      <div>
-        {amiibos?.amiibo?.map(amiibo => 
-          <div key={amiibo.id}>
-            <button onClick={() => handleAmiiboClick(amiibo)}> {amiibo.name} </button> 
+      <div className='amiiboListContainer'>
+        {amiibos?.amiibo?.map((amiibo, index) => 
+          <div className="card" key= {index }>
+            <div className="card-content">
+                <div className='card-image-container'>
+                  <img src={amiibo.image} className='card-image'/>
+                </div>
+                <div className="card-title">{amiibo.name}</div>
+                <div className="card-price">
+                  $450
+                </div>
+                <Button onClick={()=>handleAmiiboClick(amiibo)}>Comprar</Button>
+            </div>
+            
           </div>
         )}
       </div>
@@ -28,6 +43,7 @@ class Home extends React.Component {
 };
 
 const mapStateToProps  = (state) => ({ amiibos:state.amiibos, cartItems: state.cartItems});
+
 const mapDispatchToProps= (dispatch)=> { 
   return{
       getAmiibos: ()=> {dispatch(getAmiibos())},
